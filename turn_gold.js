199fa8fe8d9f214bdf590f524e266720e4510c54
asela-sandaruwan <131518724+asela-sandaruwan@users.noreply.github.com>
2025-11-08 11:55:16 +0530

    Update turn_gold.js

--- turn_gold.js
+++ turn_gold.js
@@ -1,3 +1,4 @@
 (function() {
-  document.body.style.backgroundColor = 'gold';
+  //document.body.style.backgroundColor = 'gold';
+  document.querySelectorAll('a').forEach(a=>a.style.background='yellow');
 })();
